5bc63e23b52864659221aa5d5a60000adc6c1c38
Marvin Killing <marvinkilling@gmail.com>
2015-05-29 00:38:12 +0200

    add description of timestampSeparator to example

--- example/index.js
+++ example/index.js
@@ -14,6 +14,11 @@ angular
       // if not specified, default value being used is: '' (matches bucket's root directory)
       dir: 'tmp',
 
+      // You can pick a different separator string that goes in between upload_datetime and filename_with_extension:
+      // http://<your_bucket>.s3.amazonaws.com/<dir>/<upload_datetime><this_value><filename_with_extension>
+      // if not specified, the default value being used is: '$'
+      timestampSeparator: '_',
+
       // headers which should (headersSigned) and should not (headersCommon) be signed by your private key
       // for details, visit http://docs.aws.amazon.com/AmazonS3/latest/API/RESTObjectPUT.html
       headersCommon: {
