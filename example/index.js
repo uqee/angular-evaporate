6244f47f71b8973726c5043f11521cb638ab0c32
Denis Zhbankov <denis.zhbankov@gmail.com>
2016-05-13 14:44:56 +0300

    separate configs

--- example/index.js
+++ example/index.js
@@ -2,59 +2,72 @@
 'use strict';
 angular
   .module('app', ['evaporate'])
+  .controller('AppCtrl', ['$scope',
+    function ($scope) {
 
-  // it's absolutely essential to config the module beforehand
-  // these options are passed directly into EvaporateJS itself
-  // for details look at: https://github.com/TTLabs/EvaporateJS
-  .config(['evaProvider', function (evaProvider) {
-    evaProvider.config({
-      signerUrl: '/signer',
-      aws_key: 'AKIAI6HJQ7BXS3WOAP5A',
-      bucket: 'motoroller',
-      logging: false
-    });
-  }])
+      // it's absolutely essential to config the module beforehand
+      // these options are passed directly into EvaporateJS itself
+      // for details look at: https://github.com/TTLabs/EvaporateJS
+      $scope.evaConfigInit = {
+        bucket: 'motoroller',
+        aws_key: 'AKIAI6HJQ7BXS3WOAP5A',
+        signerUrl: '/signer',
+        logging: false
+      };
 
-  .controller('AppCtrl', ['$scope', function ($scope) {
+      $scope.evaConfigAdd = {
 
-    // this variable is used like a model for particular directive
-    // all parameters here are optional
-    $scope.evaData = {
-      
-      // every file will get the following link on s3:
-      // http://<your_bucket>.s3.amazonaws.com/<this_value>/<upload_datetime>$<filename_with_extension>
-      // if you want to put the files into nested folder, just use dir: 'path/to/your/folder'
-      // if not specified, default value being used is: '' (matches bucket's root directory)
-      dir: 'tmp',
+        // headers which should (headersSigned) and should not (headersCommon) be signed by your private key
+        // for details, visit http://docs.aws.amazon.com/AmazonS3/latest/API/RESTObjectPUT.html
+        notSignedHeadersAtInitiate: {
+          'Cache-Control': 'max-age=3600'
+        },
+        xAmzHeadersAtInitiate: {
+          'x-amz-acl': 'public-read'
+        },
 
-      // You can pick a different separator string that goes in between upload_datetime and filename_with_extension:
-      // http://<your_bucket>.s3.amazonaws.com/<dir>/<upload_datetime><this_value><filename_with_extension>
-      // if not specified, the default value being used is: '$'
-      timestampSeparator: '_',
+        // custom callbacks for onProgress and onComplete events
+        info: function (msg) { console.log(msg); }
+        /*
+        onFileprogress: function (file) {
+          console.log(
+            'onProgress || name: %s, uploaded: %f%, remaining: %d seconds',
+            file.name,
+            file.progress,
+            file.timeLeft
+          );
+        },
+        onFileComplete: function (file) {
+          console.log('onComplete || name: %s', file.name);
+        },
+        onFileError: function (file, message) {
+          console.log('onError || message: %s', message);
+        }
+        */
+      };
 
-      // headers which should (headersSigned) and should not (headersCommon) be signed by your private key
-      // for details, visit http://docs.aws.amazon.com/AmazonS3/latest/API/RESTObjectPUT.html
-      headersCommon: {
-        'Cache-Control': 'max-age=3600'
-      },
-      headersSigned: {
-        'x-amz-acl': 'public-read'
-      },
+      // this variable is used like a model for particular directive
+      // all parameters here are optional
+      $scope.evaConfigName = {
 
-      // custom callbacks for onProgress and onComplete events
-      onFileProgress: function (file) {
-        console.log(
-          'onProgress || name: %s, uploaded: %f%, remaining: %d seconds',
-          file.name,
-          file.progress,
-          file.timeLeft
-        );
-      },
-      onFileComplete: function (file) {
-        console.log('onComplete || name: %s', file.name);
-      },
-      onFileError: function (file, message) {
-        console.log('onError || message: %s', message);
-      }
-    };
-  }]);
+        // bucketUrl
+        
+        // every file will get the following link on s3:
+        // http://<your_bucket>.s3.amazonaws.com/<this_value>/<upload_datetime>$<filename_with_extension>
+        // if you want to put the files into nested folder, just use dir: 'path/to/your/folder'
+        // if not specified, default value being used is: '' (matches bucket's root directory)
+        directoryName: '/tmp/',
+
+        useTimestamp: true,
+        useFilename: true,
+
+        // You can pick a different separator string that goes in between upload_datetime and filename_with_extension:
+        // http://<your_bucket>.s3.amazonaws.com/<dir>/<upload_datetime><this_value><filename_with_extension>
+        // if not specified, the default value being used is: '$'
+      timestampSeparator: '-'
+      };
+
+      //
+      $scope.evaModel = {};
+    }
+  ]);
